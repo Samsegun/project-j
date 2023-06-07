62dcb6313083642aee29a3cc7399f34a41837e55
Samsegun <oyebadesamsegun@gmail.com>
2023-06-07 19:27:17 +0100

    initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,10 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        if(isAlien) {
+            System.out.println("An alien!");
+        }else {
+            System.out.println("Not an alien!");
+        }
+    }
+}
