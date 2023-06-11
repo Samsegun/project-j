2cb34f090efa15bf6c569e3baf2abd3468bcbd59
Samsegun <oyebadesamsegun@gmail.com>
2023-06-11 22:05:08 +0100

    method overloading

--- src/ExpressionsAndKeywords/MethodOverloading.java
+++ src/ExpressionsAndKeywords/MethodOverloading.java
@@ -1,4 +1,20 @@
 package ExpressionsAndKeywords;
 
 public class MethodOverloading {
+    public static void main(String[] args) {
+        System.out.println(convertToCentimeters(5, 8) + " cm");
+        System.out.println(convertToCentimeters(68) + " cm");
+    }
+
+    public static double convertToCentimeters(int heightInInches) {
+        return  heightInInches * 2.54;
+    }
+
+    public static double convertToCentimeters(int heightInFeet, int heightInInches) {
+        double inchesToFeet = heightInInches / 12d;
+        double totalHeightInFeet = heightInFeet + inchesToFeet;
+        int totalHeightInInches = (int) (totalHeightInFeet * 12);
+
+        return convertToCentimeters(totalHeightInInches);
+    }
 }
